f3f21aa9bd52575ff18d6a989fbeda7e0080cfb9
matty-bennett <bennyot10@gmail.com>
2021-04-18 14:36:57 -0600

    created database tables and seed data

---
+++ db/seeds.sql
@@ -0,0 +1,23 @@
+INSERT INTO department (name)
+VALUES
+    ('Engineering'),
+    ('Sales'),
+    ('Human Resources');
+
+INSERT INTO role (title, salary)
+VALUES
+    ('General Manager', 150,000.00),
+    ('Office Manager', 100,000.00),
+    ('Lead Engineer', 120,000.00),
+    ('Engineer', 90,000.00),
+    ('Sales Lead', 120,000.00),
+    ('Sales Associate', 90,000.00);
+
+INSERT INTO employee (first_name, last_name)
+VALUES
+    ('Jacob', 'Smith'),
+    ('Samantha', 'Stewart'),
+    ('Jonathan', 'Newbold'),
+    ('Alfred', 'Remington'),
+    ('Lexie', 'Arlington'),
+    ('Shaun', 'Yuri');
